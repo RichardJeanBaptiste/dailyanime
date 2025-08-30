2197b7996f0cb1d8a2ed10635aca82a2826ac603
Richard Jean-Baptiste <Richinbk1@gmail.com>
2025-08-30 13:27:39 -0400

    added add and delete query routes

--- db.js
+++ db.js
@@ -10,3 +10,11 @@ const pool = new Pool({
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
+
+/**
+ * pg_dump -h localhost -U your_username -d daily_anime > daily_anime_dump.sql
+ * 
+ * createdb -h new_host -U new_username daily_anime
+ * 
+ * psql -h new_host -U new_username -d daily_anime < daily_anime_dump.sql
+ */
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('./db')
-const path = require("path")
+const path = require('path')
 const bodyParser = require('body-parser')
 const port = 3000
 
@@ -22,15 +22,41 @@ app.get('/test', async (req,res) => {
 })
 
 app.post('/add_quote', async (req, res) => {
-    const sqlQuery = `
+
+    const insertQuery = `
         INSERT INTO quotes(quote, character, anime)
         VALUES ($1, $2, $3)
         RETURNING id;
     `;
 
-    const { quote , character, anime } = req.body;
-    const result = await db.query(sqlQuery, [quote, character, anime]);
-    res.status(201).json(result.rows[0]);
+    try {
+        const { quote , character, anime, links } = req.body;
+        const result = await db.query(insertQuery, [quote, character, anime]);
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error("Database error:", error);
+        res.status(500).json({message: "An error occured while trying to Add a quote to the database."});
+    }
+})
+
+app.post('/delete_quote', async (req, res) => {
+    const deleteQuery = `
+        DELETE FROM quotes
+        WHERE quote = $1
+    `;
+
+    try {
+        const result = await db.query(deleteQuery, [req.body.quote]);
+
+        if(result.rowCount > 0){
+            res.status(200).json({message : "Quote deleted successfully."});
+        } else {
+            res.status(404).json({message: "Quote not found."});
+        }
+    } catch (error) {
+        console.error("Database error:", error);
+        res.status(500).json({message: "An error occurred while trying to delete a query"});
+    }
 })
 
 app.listen(port, () => {
